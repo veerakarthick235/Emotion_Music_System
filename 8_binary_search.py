d63c109f6a27e6f95f1d2014184ae3ce03a59f17
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2025-11-16 09:24:08 +0530

    Delete 8_binary_search.py

--- 8_binary_search.py
+++
@@ -1,9 +0,0 @@
-def bs(a,k):
-    l,h=0,len(a)-1
-    while l<=h:
-        m=(l+h)//2
-        if a[m]==k: return m
-        if k<a[m]: h=m-1
-        else: l=m+1
-    return -1
-print(bs([10,20,30,40,50],40))
