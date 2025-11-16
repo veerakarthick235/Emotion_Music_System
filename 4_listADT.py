cc6b1d628d0a838bd0a1574e5890d398310ba24c
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2025-11-16 09:22:46 +0530

    Delete 4_listADT.py

--- 4_listADT.py
+++
@@ -1,8 +0,0 @@
-class ListADT:
-    def __init__(self): self.data=[]
-    def insert(self,x): self.data.append(x)
-    def delete(self,x):
-        if x in self.data: self.data.remove(x)
-    def display(self): print(self.data)
-
-lst=ListADT(); lst.insert(10); lst.insert(20); lst.display()
