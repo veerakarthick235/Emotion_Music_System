f24c79b67866524449ca612c96f6b37bbb66839e
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2025-11-16 09:21:56 +0530

    Delete 13_bfs.py

--- 13_bfs.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-g={'A':['B','C'],'B':['D','E'],'C':['F'],'D':[],'E':[],'F':[]}
-def bfs(s):
-    vis=set(); q=deque([s])
-    while q:
-        n=q.popleft()
-        if n not in vis:
-            print(n,end=" "); vis.add(n); q.extend(g[n])
-bfs('A')
